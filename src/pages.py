e9af32dda12c1dfda313ab66b47df9a0347b484a
Yunis Huseynzade <yunisdev.04@gmail.com>
2020-09-22 23:39:29 +0400

    schema update

--- src/pages.py
+++ src/pages.py
@@ -7,14 +7,14 @@ pages = Blueprint('pages', __name__, template_folder='../public')
 
 @pages.route('/')
 def index():
-    with open('./src/page_contents/contents.json', 'rt') as source:
+    with open('./src/page_contents/contents_en.json', 'rt') as source:
         h = getContentHTML(json.load(source)['index'])
         return render_template('views/index.html', htmlFromContent=h)
 
 
 @pages.route('/about')
 def about():
-    with open('./src/page_contents/contents.json', 'rt') as source:
+    with open('./src/page_contents/contents_en.json', 'rt') as source:
         h = getContentHTML(json.load(source)['about'])
         return render_template('views/about.html', htmlFromContent=h)
 
@@ -66,7 +66,7 @@ def contact():
 
 @pages.route('/pricing')
 def pricing():
-    with open('./src/page_contents/contents.json', 'rt') as source:
+    with open('./src/page_contents/contents_en.json', 'rt') as source:
         h = getContentHTML(json.load(source)['pricing'])
         return render_template('views/pricing.html', htmlFromContent=h)
 
