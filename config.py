93de038c993d8f07f7e8cbfec3dc5d6a925d7679
Yunis Huseynzade <yunisdev.04@gmail.com>
2020-10-05 19:59:54 +0400

    config

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 import os
 DEBUG = True
 PORT = os.environ.get('FLASK_PORT', 5000)
-SECRET_KEY = os.environ.get('KRITIBUY_SECRET','hello')
+SECRET_KEY = os.environ.get('KRITIBUY_SECRET','token')
+WEBHOOK_AUTH = os.environ.get('WEBHOOK_AUTH','token')
