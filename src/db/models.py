32da33f1f0c0306a5be3df19e53975283caae69e
Yunis Huseynzade <yunisdev.04@gmail.com>
2020-10-24 14:45:15 +0400

    schema remake

--- src/db/models.py
+++ src/db/models.py
@@ -20,18 +20,28 @@ db_string = config.DB_CONN_STRING
 
 db = create_engine(db_string)
 
-base = declarative_base()
+
+class Mixin(object):
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    createdAt = Column(DateTime, nullable=False,
+                       default=datetime.datetime.utcnow())
+    updatedAt = Column(DateTime, nullable=False,
+                       default=datetime.datetime.utcnow(), onupdate=datetime.datetime.utcnow())
+
+    def __repr__(self):
+        return f'<{self.__class__.__name__} id="{self.id}">'
+
+
+base = declarative_base(cls=Mixin)
 
 
 class Permission(base):
     __tablename__ = 'Permissions'
-    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     name = Column(String(100), nullable=False)
 
 
 class Country(base):
     __tablename__ = 'Countries'
-    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     name = Column(String, nullable=False)
     alpha2 = Column(String(5), nullable=False)
     alpha3 = Column(String(5), nullable=False)
@@ -40,7 +50,6 @@ class Country(base):
 
 class City(base):
     __tablename__ = 'Cities'
-    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     name = Column(String, nullable=False)
     country = Column(Integer, ForeignKey(Country.id),
                      nullable=False)
@@ -48,7 +57,6 @@ class City(base):
 
 class User(base):
     __tablename__ = 'Users'
-    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     username = Column(String(100), unique=True, nullable=False)
     password = Column(String, nullable=False)
     email = Column(String(255), unique=True, nullable=False)
@@ -60,8 +68,6 @@ class User(base):
     city = Column(Integer, ForeignKey(City.id), nullable=False)
     brandLogoPath = Column(String, nullable=True)
     brandName = Column(String, unique=True, nullable=True)
-    createdAt = Column(DateTime, nullable=False,
-                       default=datetime.datetime.utcnow())
     brandProductTypes = Column(ARRAY(String), nullable=True)
     active = Column(Boolean, default=False, nullable=False)
     confirmationKey = Column(String, nullable=False, default='con_key')
@@ -70,14 +76,12 @@ class User(base):
 
 class AuthToken(base):
     __tablename__ = 'AuthTokens'
-    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     user = Column(Integer, ForeignKey(User.id), nullable=False)
     token = Column(String, nullable=False)
 
 
 class Payment(base):
     __tablename__ = 'Payments'
-    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     fromUser = Column(Integer, ForeignKey(User.id), nullable=False)
     toUser = Column(Integer, ForeignKey(User.id), nullable=False)
     amount = Column(Float, nullable=False)
@@ -86,7 +90,6 @@ class Payment(base):
 
 class Report(base):
     __tablename__ = 'Reports'
-    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     reporter = Column(Integer, ForeignKey(User.id), nullable=False)
     header = Column(String, nullable=False)
     body = Column(String, nullable=False)
@@ -95,23 +98,19 @@ class Report(base):
 
 class Message(base):
     __tablename__ = 'Messages'
-    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     user = Column(Integer, ForeignKey(User.id), nullable=False)
     type = Column(String(4), nullable=False)
     message = Column(String, nullable=False)
-    date = Column(DateTime, default=datetime.datetime.utcnow(), nullable=False)
 
 
 class Wallet(base):
     __tablename__ = 'Wallets'
-    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     balance = Column(Float, default=0.0, nullable=False)
     owner = Column(Integer, ForeignKey(User.id), nullable=False)
 
 
 class OrderInfo(base):
     __tablename__ = 'OrderInfos'
-    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     user = Column(Integer, ForeignKey(User.id))
     address = Column(String, nullable=False)
     city = Column(Integer, ForeignKey(City.id), nullable=False)
@@ -122,24 +121,20 @@ class OrderInfo(base):
 
 class Order(base):
     __tablename__ = 'Orders'
-    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     orderedTo = Column(Integer, ForeignKey(User.id), nullable=False)
     orderedBy = Column(Integer, ForeignKey(User.id), nullable=False)
     orderedProduct = Column(String, nullable=False)
     orderText = Column(String, nullable=False)
     info = Column(Integer, ForeignKey(OrderInfo.id), nullable=False)
     done = Column(Boolean, default=False, nullable=False)
-    date = Column(DateTime, default=datetime.datetime.utcnow(), nullable=False)
 
 
 class OrderRating(base):
     __tablename__ = 'OrderRatings'
-    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     value = Column(Integer, nullable=False)
     byUser = Column(Integer, ForeignKey(User.id), nullable=False)
     toUser = Column(Integer, ForeignKey(User.id), nullable=False)
     forOrder = Column(Integer, ForeignKey(Order.id), nullable=False)
-    date = Column(DateTime, default=datetime.datetime.utcnow(), nullable=False)
 
 
 Session = sessionmaker(db)
