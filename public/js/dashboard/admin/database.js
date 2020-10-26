afa0f8106d7847166971eda02734f57965dcee78
Yunis Huseynzade <yunisdev.04@gmail.com>
2020-10-26 12:38:38 +0400

    new colors

---
+++ public/js/dashboard/admin/database.js
@@ -0,0 +1,6 @@
+document.querySelectorAll('.delete-row-btn').forEach(i => {
+    i.addEventListener('click', (e) => {
+        var clickedID = e.target.parentNode.parentNode.parentNode.getAttribute('key')
+        
+    })
+})
