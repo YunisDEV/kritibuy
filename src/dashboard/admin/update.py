00fe8bb737bb754adadff95cade148033aa2edf3
Yunis Huseynzade <yunisdev.04@gmail.com>
2020-11-12 23:55:28 +0400

    countries update fix

--- src/dashboard/admin/update.py
+++ src/dashboard/admin/update.py
@@ -1,17 +1,28 @@
 from sqlalchemy import text
 from ...db import *
 from flask import make_response
+import config
+import os
+from werkzeug.utils import secure_filename
+
 
 def countries_update(request):
     try:
         id = request.args.get('id', '')
         if not id:
             raise Exception('ID is not in arguments')
-        country = session.query(Country).filter(Country.id==int(id)).one()
+        country = session.query(Country).filter(Country.id == int(id)).one()
         country.name = request.form["name"]
         country.alpha2 = request.form["alpha2"]
         country.alpha3 = request.form["alpha3"]
-        country.flagPath = request.form["flagPath"]
+        flag = request.files.get('flagPath', None)
+        print(flag)
+        if flag:
+            flag_directory = os.path.join(config.UPLOAD_DIR_COUNTRY_FLAGS, str(
+                request.form["alpha3"].lower())+'_flag.'+secure_filename(flag.filename).split('.')[-1])
+            flag.save(flag_directory)
+            flag_dir = flag_directory.split('public')[1]
+            country.flagPath = flag_dir
         session.commit()
         resp = make_response({"success": True}, 200)
         return resp
