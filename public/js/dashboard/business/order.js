526cb03cd9783d8bf7e535d4dac5f0118234ae00
Yunis Huseynzade <yunisdev.04@gmail.com>
2020-11-16 22:35:39 +0400

    business admin panel improvements

--- public/js/dashboard/business/order.js
+++ public/js/dashboard/business/order.js
@@ -1,1 +1,22 @@
-console.log('a')
+var qs = document.querySelector.bind(document)
+var qsa = document.querySelectorAll.bind(document)
+qsa('.delete-order-btn').forEach(i => {
+    i.addEventListener('click', (e) => {
+        e.preventDefault()
+        var clickedID = e.target.parentNode.parentNode.parentNode.getAttribute('key')
+        var d = confirm('Do you want to delete order id: ' + clickedID + '?\nNote: Customer will get message about order deletion.')
+        if (d) {
+            
+        }
+    })
+})
+qsa('.done-order-btn').forEach(i => {
+    i.addEventListener('click', (e) => {
+        e.preventDefault()
+        var clickedID = e.target.parentNode.parentNode.parentNode.getAttribute('key')
+        var d = confirm('Do you want to make order id: ' + clickedID + ' done?\nNote: Customer will get message order is done.')
+        if (d) {
+            
+        }
+    })
+})
