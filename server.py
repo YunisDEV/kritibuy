447a393340466ee061cf2d5743e9015e43407063
Yunis Huseynzade <yunisdev.04@gmail.com>
2020-11-29 14:46:16 +0400

    app

--- server.py
+++ server.py
@@ -1,5 +1,4 @@
-from src import create_app
+from src import app
 import config
-app = create_app()
 if __name__ == "__main__":
     app.run(debug=config.DEBUG,port=config.PORT,host=config.HOST)
--- src/__init__.py
+++ src/__init__.py
@@ -7,21 +7,18 @@ from werkzeug.exceptions import HTTPException
 import flask_assets
 import config
 
+app = Flask(__name__, static_folder='../public',
+            static_url_path='/', template_folder='./templates')
 
-def create_app():
-    app = Flask(__name__, static_folder='../public',
-                static_url_path='/', template_folder='./templates')
+app.register_blueprint(views)
+app.register_blueprint(dashboard, url_prefix='/dashboard')
+app.register_blueprint(account)
+app.register_blueprint(chatbot)
 
-    app.register_blueprint(views)
-    app.register_blueprint(dashboard, url_prefix='/dashboard')
-    app.register_blueprint(account)
-    app.register_blueprint(chatbot)
+assets = flask_assets.Environment()
+assets.init_app(app)
 
-    assets = flask_assets.Environment()
-    assets.init_app(app)
 
-    @app.errorhandler(HTTPException)
-    def error_page_handler(e):
-        return render_template('error_page.html', code=e.code, name=e.name), e.code
-
-    return app
+@app.errorhandler(HTTPException)
+def error_page_handler(e):
+    return render_template('error_page.html', code=e.code, name=e.name), e.code
